a28d1b4e4f6f5c76ee72ff85fa82a930fac997b8
russhams <105001079+russhams@users.noreply.github.com>
2022-05-09 16:09:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def print_hello(name):
+    st.write(f"## Hello, {name}!")
+
+print_hello("Anna")
+
+# if __name__ == "__main__":
+#     name = input("Enter your name: ")
+#     print_hello(name)
