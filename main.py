68c8e1bb8af10860526b4007d0c10ede94a7f2f1
russhams <105001079+russhams@users.noreply.github.com>
2022-05-09 16:12:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 def print_hello(name):
     st.write(f"## Hello, {name}!")
 
-print_hello("Anna")
+print_hello("Misha")
 
 # if __name__ == "__main__":
 #     name = input("Enter your name: ")
